bf1b9127f48637292c1ac08740bd0a782f137be6
Nungari-Stack <nungarimaina10@gmail.com>
2025-08-11 21:10:18 +0300

    Updates

---
+++ PRACTICE/persona; practice.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number"))
+num2 = float(input("Enter the  second number"))
+difference_result = (num1 - num2)
+sum_result = (num2 + num2)
+quotient_result = (num1 / num2)
+product_result = (num1 * num2)
+
+print(f"Results of your two numbers:")
+print(f"{difference_result}")
+print(f"{sum_result}")
+print(f"{quotient_result}")
+print(f"{product_result}")
+
+
